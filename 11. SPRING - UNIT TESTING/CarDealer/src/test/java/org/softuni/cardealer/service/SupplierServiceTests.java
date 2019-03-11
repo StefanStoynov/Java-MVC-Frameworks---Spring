84d808fd655341e8465b114db52b75f935f11f3c
Stefan Stoynov <sstoynov87@gmail.com>
2019-03-11 10:46:01 +0200

    commit testing of supplier service

--- 11. SPRING - UNIT TESTING/CarDealer/src/test/java/org/softuni/cardealer/service/SupplierServiceTests.java
+++ 11. SPRING - UNIT TESTING/CarDealer/src/test/java/org/softuni/cardealer/service/SupplierServiceTests.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.modelmapper.ModelMapper;
+import org.softuni.cardealer.domain.entities.Supplier;
 import org.softuni.cardealer.domain.models.service.SupplierServiceModel;
 import org.softuni.cardealer.repository.SupplierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +24,7 @@ public class SupplierServiceTests {
     private ModelMapper modelMapper;
 
     @Before
-    public void init(){
+    public void init() {
         this.modelMapper = new ModelMapper();
     }
 
@@ -43,8 +44,9 @@ public class SupplierServiceTests {
         Assert.assertEquals(expected.getName(), actual.getName());
         Assert.assertEquals(expected.isImporter(), actual.isImporter());
     }
+
     @Test(expected = Exception.class)
-    public void supplierService_saveSupplierWithNullValues_TrowsException(){
+    public void supplierService_saveSupplierWithNullValues_TrowsException() {
         SupplierService supplierService = new SupplierServiceImpl(this.supplierRepository, this.modelMapper);
 
         SupplierServiceModel toBeSaved = new SupplierServiceModel();
@@ -54,4 +56,106 @@ public class SupplierServiceTests {
         supplierService.saveSupplier(toBeSaved);
     }
 
+    @Test
+    public void supplierService_editSupplierwithCorrectValues_returnCorrect() {
+        SupplierService supplierService = new SupplierServiceImpl(this.supplierRepository, this.modelMapper);
+
+        Supplier supplier = new Supplier();
+        supplier.setName("Gosho");
+        supplier.setImporter(true);
+
+        supplier = this.supplierRepository.saveAndFlush(supplier);
+
+        SupplierServiceModel toBeEditted = new SupplierServiceModel();
+        toBeEditted.setId(supplier.getId());
+        toBeEditted.setName("Pesho");
+        toBeEditted.setImporter(false);
+
+        SupplierServiceModel actual = supplierService.editSupplier(toBeEditted);
+        SupplierServiceModel expected = this.modelMapper.map(this.supplierRepository.findAll().get(0), SupplierServiceModel.class);
+
+        Assert.assertEquals(expected.getId(), actual.getId());
+        Assert.assertEquals(expected.getName(), actual.getName());
+        Assert.assertEquals(expected.isImporter(), actual.isImporter());
+    }
+
+    @Test(expected = Exception.class)
+    public void supplierService_editSupplierServiceWithNullValues_TrowsException(){
+        SupplierService supplierService = new SupplierServiceImpl(this.supplierRepository, this.modelMapper);
+
+        Supplier supplier = new Supplier();
+        supplier.setName("Gosho");
+        supplier.setImporter(true);
+
+        supplier = this.supplierRepository.saveAndFlush(supplier);
+
+        SupplierServiceModel toBeEditted = new SupplierServiceModel();
+        toBeEditted.setId(supplier.getId());
+        toBeEditted.setName(null);
+        toBeEditted.setImporter(false);
+
+        supplierService.editSupplier(toBeEditted);
+    }
+
+    @Test
+    public void supplierService_deleteSupplierWithCorrectValues_returnCorrect(){
+        SupplierService supplierService = new SupplierServiceImpl(this.supplierRepository, this.modelMapper);
+
+        Supplier supplier = new Supplier();
+        supplier.setName("Gosho");
+        supplier.setImporter(true);
+
+        supplier = this.supplierRepository.saveAndFlush(supplier);
+
+        supplierService.deleteSupplier(supplier.getId());
+
+        long expected = 0;
+        long actual = supplierRepository.count();
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test(expected = Exception.class)
+    public void supplierService_deleteSupplierServiceWithNullValues_TrowsException() {
+        SupplierService supplierService = new SupplierServiceImpl(this.supplierRepository, this.modelMapper);
+
+        Supplier supplier = new Supplier();
+        supplier.setName("Gosho");
+        supplier.setImporter(true);
+
+        supplier = this.supplierRepository.saveAndFlush(supplier);
+
+        supplierService.deleteSupplier("InvalidId");
+    }
+
+    @Test
+    public void supplierService_findSupplierWithCorrectValues_returnCorrect() {
+        SupplierService supplierService = new SupplierServiceImpl(this.supplierRepository, this.modelMapper);
+
+        Supplier supplier = new Supplier();
+        supplier.setName("Gosho");
+        supplier.setImporter(true);
+
+        supplier = this.supplierRepository.saveAndFlush(supplier);
+
+        SupplierServiceModel actual = supplierService.findSupplierById(supplier.getId());
+        SupplierServiceModel expected = this.modelMapper.map(supplier, SupplierServiceModel.class);
+
+        Assert.assertEquals(expected.getId(), actual.getId());
+        Assert.assertEquals(expected.getName(), actual.getName());
+        Assert.assertEquals(expected.isImporter(), actual.isImporter());
+    }
+
+    @Test(expected = Exception.class)
+    public void supplierService_findSupplierServiceWithNullValues_TrowsException() {
+        SupplierService supplierService = new SupplierServiceImpl(this.supplierRepository, this.modelMapper);
+
+        Supplier supplier = new Supplier();
+        supplier.setName("Gosho");
+        supplier.setImporter(true);
+
+        supplier = this.supplierRepository.saveAndFlush(supplier);
+        supplierService.findSupplierById("notValidId");
+    }
 }
+
