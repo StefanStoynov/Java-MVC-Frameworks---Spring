f339afa77f7a028e50db628f6331eaa5463bfd81
Stefan Stoynov <sstoynov87@gmail.com>
2019-03-17 22:25:58 +0200

    create validations of binding model

--- 03. SPRING BOOT MVC - THYMELEAF ENGINE/residentevil/src/main/java/residentevil/domain/models/binding/VirusAddBindingModel.java
+++ 03. SPRING BOOT MVC - THYMELEAF ENGINE/residentevil/src/main/java/residentevil/domain/models/binding/VirusAddBindingModel.java
@@ -1,12 +1,12 @@
 package residentevil.domain.models.binding;
 
+import jdk.jfr.BooleanFlag;
 import org.springframework.format.annotation.DateTimeFormat;
 import residentevil.domain.entities.Capital;
 import residentevil.domain.entities.Magnitude;
 import residentevil.domain.entities.Mutation;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -29,20 +29,25 @@ public class VirusAddBindingModel {
     public VirusAddBindingModel() {
     }
 
-    @NotNull()
+    @NotNull(message = "Name cannot be empty!")
     @Size(min = 3, max = 10, message = "Name length should be between 3 and 10 symbols")
     public String getName() {
         return this.name;
     }
 
+    @Size(min = 5, max = 100, message = "Description length should be between 5 and 100 symbols")
     public String getDescription() {
         return this.description;
     }
 
+    @NotNull(message = "Side Effects cannot be empty!")
+    @Size(max = 50, message = "Side Effects length should not more than 50 symbols")
     public String getSideEffects() {
         return this.sideEffects;
     }
 
+    @NotNull(message = "Creator cannot be empty!")
+    @Pattern(regexp = "Corp|corp", message = "Creator must be Corp or corp!")
     public String getCreator() {
         return this.creator;
     }
@@ -59,23 +64,31 @@ public class VirusAddBindingModel {
         return this.mutation;
     }
 
+    @NotNull(message = "Turn over rate cannot be empty!")
+    @Min(value = 0, message = "Turn over rate minimum is 0")
+    @Max(value = 100, message = "Turn over rate maximum is 100")
     public Integer getTurnoverRate() {
         return this.turnoverRate;
     }
 
+    @NotNull(message = "Hours until turn cannot be empty!")
+    @Min(value = 1, message = "Hours until turn minimum is 1")
+    @Max(value = 12, message = "Hours until turn minimum is 12")
     public Integer getHoursUntilTurn() {
         return this.hoursUntilTurn;
     }
 
+    @NotNull(message = "Magnitude cannot be empty!")
     public Magnitude getMagnitude() {
         return this.magnitude;
     }
 
+    @NotNull(message = "Date cannot be empty!")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     public LocalDate getReleasedOn() {
         return this.releasedOn;
     }
-
+    @NotNull(message = "Cannot be empty!")
     public List<String> getCapitals() {
         return this.capitals;
     }
